5972ff69682b3c93ba3b8c9a0d34f6a4c4dc593e
haoxi <3020001195@tju.edu.cn>
2023-12-30 11:35:59 +0800

    update oi-wiki spider

--- data_spider.py
+++ data_spider.py
@@ -88,7 +88,7 @@ def wiki_spider():
             res.read().decode('utf-8').split("<div class=md-content data-md-component=content>")[1].split(
                 "</div></main>")[0]
         name = html.split('<h1>')[1].split('</h1>')[0]
-        htmls.append([url, name, html.lower()])
+        htmls.append([url, name, html.lower().replace('\n','').replace(' ','')])
         # print(f"-----get htmls {cnt}/{len(urls)}-----")
     # print("-----get htmls finished-----")
 
@@ -96,77 +96,88 @@ def wiki_spider():
 
 
 def spider_main():
-    """爬取洛谷"""
-    print("正在爬取洛谷...")
-    start_luogu = time.time()
-    if not os.path.exists('./data/'):
-        os.makedirs('./data/')
-
-    luogu_page_cnt = 0  # 爬取洛谷的页数
-    for page in range(1, 189):
-        try:
-            luogu_url = f"https://www.luogu.com.cn/problem/list?page={page}"
-            data = luogu_spider(luogu_url)
-
-            # 逐个将爬取的数据保存为单独的JSON对象
-            for item in data:
-                with open('./data/luogu.json', 'a', encoding='utf-8') as f:
-                    json.dump(item, f, ensure_ascii=False)
-                    f.write('\n')  # 每个JSON对象后面添加一个换行符
-            luogu_page_cnt += 1
-        except Exception as e:
-            print(e)
-
-    end_luogu = time.time()
-    luogu_time = end_luogu - start_luogu
-    print(f"----------洛谷爬取完成！爬取{luogu_page_cnt}页，用时{int(luogu_time)}秒。----------")
-
-    """从luogu.json中生成algorithm.dict，用于爬取oi-wiki"""
-    all_algorithms = set()
-    with open('data/luogu.json', 'r', encoding='utf-8') as file:
-        for data in file:
-            data_json = json.loads(data)
-            algorithm = data_json.get('algorithm', [])
-            all_algorithms.update(algorithm)
-
-    # 去除空值
-    all_algorithms.discard('')
-
-    # 将算法写入 algorithm.txt 文件
-    if not os.path.exists('dict'):
-        os.makedirs('dict')
-    with open('dict/algorithm.txt', 'w', encoding='utf-8') as txt_file:
-        for algorithm in all_algorithms:
-            txt_file.write(f"{algorithm}\n")
+    # """爬取洛谷"""
+    # print("正在爬取洛谷...")
+    # start_luogu = time.time()
+    # if not os.path.exists('./data/'):
+    #     os.makedirs('./data/')
+    #
+    # luogu_page_cnt = 0  # 爬取洛谷的页数
+    # for page in range(1, 189):
+    #     try:
+    #         luogu_url = f"https://www.luogu.com.cn/problem/list?page={page}"
+    #         data = luogu_spider(luogu_url)
+    #
+    #         # 逐个将爬取的数据保存为单独的JSON对象
+    #         for item in data:
+    #             with open('./data/luogu.json', 'a', encoding='utf-8') as f:
+    #                 json.dump(item, f, ensure_ascii=False)
+    #                 f.write('\n')  # 每个JSON对象后面添加一个换行符
+    #         luogu_page_cnt += 1
+    #     except Exception as e:
+    #         print(e)
+    #
+    # end_luogu = time.time()
+    # luogu_time = end_luogu - start_luogu
+    # print(f"----------洛谷爬取完成！爬取{luogu_page_cnt}页，用时{int(luogu_time)}秒。----------")
+    #
+    # """从luogu.json中生成algorithm.dict，用于爬取oi-wiki"""
+    # all_algorithms = set()
+    # with open('data/luogu.json', 'r', encoding='utf-8') as file:
+    #     for data in file:
+    #         data_json = json.loads(data)
+    #         algorithm = data_json.get('algorithm', [])
+    #         all_algorithms.update(algorithm)
+    #
+    # # 去除空值
+    # all_algorithms.discard('')
+    #
+    # # 将算法写入 algorithm.txt 文件
+    # if not os.path.exists('dict'):
+    #     os.makedirs('dict')
+    # with open('dict/algorithm.txt', 'w', encoding='utf-8') as txt_file:
+    #     for algorithm in all_algorithms:
+    #         txt_file.write(f"{algorithm}\n")
 
     """爬取oi-wiki"""
     print("正在爬取oi-wiki...")
+    spider_web = False  # 是否爬取oi-wiki的网页，如果已经爬过一次，有了./data/webs.txt就设置为False
     start_wiki = time.time()
     algorithm_dict_file = open('./dict/algorithm.txt', 'r', encoding='utf-8')
     algorithm_dict = algorithm_dict_file.readlines()
     algorithm_dict_file.close()
-    res = wiki_spider()
-    print("html爬取完成，准备根据算法匹配html")
+    if spider_web:
+        res = wiki_spider()
+        print("html爬取完成，准备根据算法匹配html")
+        webs = open('./data/webs.txt', 'w', encoding='utf-8')
+        webs.write('\n'.join(i[0]+"::::::::" + i[1] + "::::::::" + i[2] for i in res))
+        webs.close()
+    webs = open('./data/webs.txt', 'r', encoding='utf-8')
+    lines = webs.readlines()
+    webs.close()
+    res = []
+    for i in lines:
+        res.append(i.split('::::::::'))
     all_data = []
     for algorithm in algorithm_dict:  # 枚举算法
         explains = []
         algorithm = algorithm.replace('\n', '')
-        # algorithm_s = algorithm.split(',')
+        algorithm_s = algorithm.split(',')
         records = {}
         urls = {}
-        # for a in algorithm_s:  # 枚举一个算法可能的名字
-        for website in res:
-            similarity = fuzz.partial_ratio(algorithm.lower(), website[2])
-            if similarity > 0:
-                if website[1] in records.keys():
-                    records[website[1]] += similarity
-                else:
-                    records[website[1]] = similarity
-                    urls[website[1]] = website[0]
-            # cnt = website[2].count(a.lower())
-            # if cnt >= 5:  # 算法词在网页出现的频繁度 超参数，可设置
+        for a in algorithm_s:  # 枚举一个算法可能的名字
+            a = a.replace(' ', '').split('（')[0]
+            for website in res:
+                # similarity = fuzz.partial_ratio(algorithm.lower(), website[2])
+                cnt = website[2].count(a.lower())
+                if cnt > 0:
+                    if website[1] in records.keys():
+                        records[website[1]] += cnt
+                    else:
+                        records[website[1]] = cnt
+                        urls[website[1]] = website[0]
         for record in records.items():
-            if record[1] >= 75:
+            if record[1] >= 1:
                 explains.append({'url': urls[record[0]], 'name': record[0], 'frequency': record[1]})
         all_data.append({'algorithm': algorithm, 'explains': explains})
     end_wiki = time.time()
